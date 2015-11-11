dc5605b0b8798a0adf418a9d45fc78d389d81c8b
Satish Talluri <satish@neptune.io>
2015-11-10 16:28:09 -0800

    Update prod cli machines to pickup softlayer and tugboat

--- prod_cli_machines_setup.sh
+++ prod_cli_machines_setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Use a specific path
-PATH="/usr/bin:/usr/local/bin:/usr/local/heroku/bin:$PATH"
+PATH="/usr/bin:/usr/local/bin:/usr/local/heroku/bin:/opt/aws/bin:$PATH"
 
 # Install Neptuneio agent
 NEPTUNEIO_KEY="b7bfe04856cb4388b9c93061a2f43acb" bash -c "$(curl -sS -L https://raw.githubusercontent.com/neptuneio/nagent/prod/src/install_nagent.sh)"
 
 # Give neptuneio agent sudo permissions
-echo "neptuneioagent ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo 'Defaults:neptuneioagent !requiretty' >> /etc/sudoers
+# echo "neptuneioagent ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# echo 'Defaults:neptuneioagent !requiretty' >> /etc/sudoers
 
 # Install Heroku CLI
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
 # Install AWS CLI
 pip install -U awscli
+
+# Install Softlayer CLI
+pip install -U softlayer
+
+# Install Digital ocean tugboat CLI
+gem install tugboat
 
 # Generic PATH variable update for agent to pick up various CLIs
 echo "PATH=$PATH ; export PATH" >> ~neptuneioagent/.bashrc
