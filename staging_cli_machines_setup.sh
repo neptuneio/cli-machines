40a151ddb48a28b1711051812f60d941a199da52
Satish Talluri <satish@neptune.io>
2015-12-22 16:12:46 -0800

    Restrict commands on cli machine

--- staging_cli_machines_setup.sh
+++ staging_cli_machines_setup.sh
@@ -3,6 +3,17 @@
 # Use a specific path
 PATH="/usr/bin:/usr/local/bin:/usr/local/heroku/bin:/opt/aws/bin:$PATH"
 
+# Restrict commands
+cd /bin
+chmod 700 *
+chmod 4111 su
+chmod 755 date echo awk basename bash cut date echo egrep env false fgrep gawk grep mktemp more sed sh sleep sort true
+
+cd /usr/bin
+chmod 700 *
+chmod 4111 sudo
+chmod 755 awk aws aws_completer cut curl env gawk id less openssl python* ruby* sha* ssh* tail tty tee wc
+
 # Install Neptune agent
 AGENT_USER="neptune" END_POINT="neptune-staging-env.herokuapp.com" API_KEY="fdf59b33c66c4f3a8f1eff809249b972" bash -c "$(curl -sS -L https://raw.githubusercontent.com/neptuneio/neptune-agent/master/scripts/linux/install_neptune_agent_linux.sh)"
 
