b1a4469aa50b3bc2b41c2b15625612073505304f
Satish Talluri <satish@neptune.io>
2016-01-30 12:49:03 -0800

    Install heroku pg plugin

--- staging_cli_machines_setup.sh
+++ staging_cli_machines_setup.sh
@@ -3,17 +3,6 @@
 # Use a specific path
 PATH="/usr/bin:/usr/local/bin:/usr/local/heroku/bin:/opt/aws/bin:$PATH"
 
-# Restrict commands
-cd /bin
-chmod 700 *
-chmod 4111 su
-chmod 755 date echo awk basename bash cut date echo egrep env false fgrep gawk grep mktemp more sed sh sleep sort true
-
-cd /usr/bin
-chmod 700 *
-chmod 4111 sudo
-chmod 755 awk aws aws_completer cut curl env gawk id less openssl python* ruby* sha* ssh* tail tty tee wc
-
 # Install Neptune agent
 AGENT_USER="neptune" END_POINT="staging.neptune.io" API_KEY="fdf59b33c66c4f3a8f1eff809249b972" bash -c "$(curl -sS -L https://raw.githubusercontent.com/neptuneio/neptune-agent/master/scripts/linux/install_neptune_agent_linux.sh)"
 
@@ -23,6 +12,11 @@ AGENT_USER="neptune" END_POINT="staging.neptune.io" API_KEY="fdf59b33c66c4f3a8f1
 
 # Install Heroku CLI
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
+
+sleep 2
+
+# Install heroku pg plugin
+heroku plugins:install git://github.com/heroku/heroku-pg-extras.git
 
 # Install AWS CLI
 pip install -U awscli
@@ -36,6 +30,17 @@ gem install tugboat
 # Generic PATH variable update for agent to pick up various CLIs
 echo "PATH=$PATH ; export PATH" >> ~neptune/.bashrc
 source ~neptune/.bashrc
+
+# Restrict commands
+cd /bin
+chmod 700 *
+chmod 4111 su
+chmod 755 date echo awk basename bash cut date echo egrep env false fgrep gawk grep mktemp more sed sh sleep sort true
+
+cd /usr/bin
+chmod 700 *
+chmod 4111 sudo
+chmod 755 awk aws aws_completer cut curl env gawk id less openssl python* ruby* sha* ssh* tail tty tee wc
 
 # Restart agent
 service neptune-agentd restart
