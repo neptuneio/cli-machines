099a21b1961c3a7dca0c9d6b0b99dd0770c5ffd2
Satish Talluri <satish@neptune.io>
2016-01-30 17:24:53 -0800

    Update prod cli script with postgresql

--- prod_cli_machines_setup.sh
+++ prod_cli_machines_setup.sh
@@ -2,29 +2,28 @@
 
 CLI_AGENT_USER="neptune"
 
+# Create the new user if it does not exist
+id -u $CLI_AGENT_USER &>/dev/null || useradd $CLI_AGENT_USER -m
+
 # Use a specific path
 export PATH="/usr/bin:/usr/local/bin:/usr/local/heroku/bin:/opt/aws/bin:$PATH"
-
-# Install Neptune agent
-AGENT_USER=$CLI_AGENT_USER API_KEY="b7bfe04856cb4388b9c93061a2f43acb" bash -c "$(curl -sS -L https://raw.githubusercontent.com/neptuneio/neptune-agent/prod/scripts/linux/install_neptune_agent_linux.sh)"
-
-# Give neptune agent sudo permissions
-# echo "neptune ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-# echo 'Defaults:neptune !requiretty' >> /etc/sudoers
 
 # Install git
 yum install -y git
 
+# Install postgresql for heroku pg-extras plugin
+yum install -y postgresql
+
 # Install Heroku CLI
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
-# Install heroku pg plugin as agent user
+# Install heroku pg-extras plugin as agent user
 sleep 2
 su - $CLI_AGENT_USER -s /bin/bash -c "/usr/local/heroku/bin/heroku update"
 su - $CLI_AGENT_USER -s /bin/bash -c "/usr/local/heroku/bin/heroku plugins:install git://github.com/heroku/heroku-pg-extras.git"
 
 # Upgrade pip to avoid warnings/errors
-pip install --upgrade pip
+pip install -U pip
 
 # Install AWS CLI
 pip install -U awscli
@@ -40,6 +39,13 @@ CLI_AGENT_USER_BASHRC=`eval echo ~$CLI_AGENT_USER/.bashrc`
 echo "PATH=$PATH ; export PATH" >> $CLI_AGENT_USER_BASHRC
 source $CLI_AGENT_USER_BASHRC
 
+# Install Neptune agent
+AGENT_USER=$CLI_AGENT_USER API_KEY="b7bfe04856cb4388b9c93061a2f43acb" bash -c "$(curl -sS -L https://raw.githubusercontent.com/neptuneio/neptune-agent/prod/scripts/linux/install_neptune_agent_linux.sh)"
+
+# Give neptune agent sudo permissions
+# echo "neptune ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# echo 'Defaults:neptune !requiretty' >> /etc/sudoers
+
 # Restrict commands
 cd /bin
 chmod 700 *
@@ -49,7 +55,7 @@ chmod 755 date echo awk basename bash cut date echo egrep env false fgrep gawk g
 cd /usr/bin
 chmod 700 *
 chmod 4111 sudo
-chmod 755 awk aws aws_completer cut curl env gawk git id less openssl python* ruby* sha* ssh* tail tty tee wc
+chmod 755 awk aws aws_completer cut curl env gawk git* id less openssl python* ruby* sha* ssh* tail tty tee wc psql*
 
 # Restart agent
 service neptune-agentd restart
